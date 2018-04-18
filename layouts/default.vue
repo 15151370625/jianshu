1a2cc5b872fc4bbe30af64d8e15da1c5ee56e24e
1823499865@qq.com <1823499865@qq.com>
2018-04-18 19:19:57 +0800

    4.18下午

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue)
+// 首先引入bootstrap.css,加载bootstrap样式
+import 'bootstrap/dist/css/bootstrap.css'
+// 引入bootstrap-vue.css文件
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Hello World</p>
+  </div>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  data () {
+    return{
+      name:'首页'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
