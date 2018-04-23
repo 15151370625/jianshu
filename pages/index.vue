78f4b1bf5f91ff0d93ba54ea62ee7fbb6f15b064
1823499865@qq.com <1823499865@qq.com>
2018-04-23 18:01:58 +0800

    左侧列表布局完成

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,144 @@
             </nuxt-link>
           </div>
           <div class="split-line"></div>
+          <!-- 文章列表 -->
+          <div class="list-container">
+            <ul class="note-list">
+              <li class="have-img">
+                <nuxt-link to="/p/123" class="wrap-img">
+                  <img src="~/assets/img/note.png" alt="">
+                </nuxt-link>
+                <div class="content">
+                  <div class="author">
+                    <nuxt-link to="/p/123" class="avatar">
+                      <img src="~/assets/img/1.jpg" alt="">
+                    </nuxt-link>
+                    <div class="info">
+                        <nuxt-link to="/u/123" class="nickname">测试用户</nuxt-link>
+                        <span class="time">
+                          04.12 10:18
+                        </span>
+                    </div>
+                  </div>
+                  <nuxt-link to="/u/123" class="title">实现一个靠谱的Web认证</nuxt-link>
+                  <div class="abstruct">Web认证是任何一个认真一点的网站都必须实现的基本功能。这个功能解决了让服务器“认识你就是你“的问题。这个功能看起来貌似很简单，但是实际上处处是坑。因为认证是依靠一套技术整体...</div>
+                  <div class="meta">
+                    <nuxt-link to="/u/123" class="collection-tag">程序员</nuxt-link>
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-eye"></i>
+                      1500
+                    </nuxt-link>
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-comment"></i>
+                      100
+                    </nuxt-link>
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-heart"></i>
+                      20
+                    </nuxt-link>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="content">
+                  <div class="author">
+                    <nuxt-link to="/p/123" class="avatar">
+                      <img src="~/assets/img/1.jpg" alt="">
+                    </nuxt-link>
+                    <div class="info">
+                        <nuxt-link to="/u/123" class="nickname">测试用户</nuxt-link>
+                        <span class="time">
+                          04.12 10:18
+                        </span>
+                    </div> 
+                  </div>
+                  <nuxt-link to="/u/123" class="title">实现一个靠谱的Web认证</nuxt-link>
+                  <div class="abstruct">Web认证是任何一个认真一点的网站都必须实现的基本功能。这个功能解决了让服务器“认识你就是你“的问题。这个功能看起来貌似很简单，但是实际上处处是坑。因为认证是依靠一套技术整体...</div>
+                  <div class="meta">
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-eye"></i>
+                      1500
+                    </nuxt-link>
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-comment"></i>
+                      100
+                    </nuxt-link>
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-heart"></i>
+                      20
+                    </nuxt-link>
+                  </div>
+                </div>
+              </li>
+              <li class="have-img">
+                <nuxt-link to="/p/123" class="wrap-img">
+                  <img src="~/assets/img/note.png" alt="">
+                </nuxt-link>
+                <div class="content">
+                  <div class="author">
+                    <nuxt-link to="/p/123" class="avatar">
+                      <img src="~/assets/img/1.jpg" alt="">
+                    </nuxt-link>
+                    <div class="info">
+                        <nuxt-link to="/u/123" class="nickname">测试用户</nuxt-link>
+                        <span class="time">
+                          04.12 10:18
+                        </span>
+                    </div>
+                  </div>
+                  <nuxt-link to="/u/123" class="title">实现一个靠谱的Web认证</nuxt-link>
+                  <div class="abstruct">Web认证是任何一个认真一点的网站都必须实现的基本功能。这个功能解决了让服务器“认识你就是你“的问题。这个功能看起来貌似很简单，但是实际上处处是坑。因为认证是依靠一套技术整体...</div>
+                  <div class="meta">
+                    <nuxt-link to="/u/123" class="collection-tag">程序员</nuxt-link>
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-eye"></i>
+                      1500
+                    </nuxt-link>
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-comment"></i>
+                      100
+                    </nuxt-link>
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-heart"></i>
+                      20
+                    </nuxt-link>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="content">
+                  <div class="author">
+                    <nuxt-link to="/p/123" class="avatar">
+                      <img src="~/assets/img/1.jpg" alt="">
+                    </nuxt-link>
+                    <div class="info">
+                        <nuxt-link to="/u/123" class="nickname">测试用户</nuxt-link>
+                        <span class="time">
+                          04.12 10:18
+                        </span>
+                    </div> 
+                  </div>
+                  <nuxt-link to="/u/123" class="title">实现一个靠谱的Web认证</nuxt-link>
+                  <div class="abstruct">Web认证是任何一个认真一点的网站都必须实现的基本功能。这个功能解决了让服务器“认识你就是你“的问题。这个功能看起来貌似很简单，但是实际上处处是坑。因为认证是依靠一套技术整体...</div>
+                  <div class="meta">
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-eye"></i>
+                      1500
+                    </nuxt-link>
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-comment"></i>
+                      100
+                    </nuxt-link>
+                    <nuxt-link to="/p/123">
+                      <i class="fa fa-heart"></i>
+                      20
+                    </nuxt-link>
+                  </div>
+                </div>
+              </li>
+            </ul>
+            <a href="#" class="load-more">阅读更多</a>
+          </div>
         </div>
         <div class="col-xs-offset-1 col-xs-7 aside">
           侧边栏
